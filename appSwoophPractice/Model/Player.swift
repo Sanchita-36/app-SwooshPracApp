c42a3854b2cbab387c9138050eb0cc812ff66cc3
sanchita <mac@Macs-MacBook-Pro.local>
2021-03-27 20:16:48 +0530

    Initial code for demo app ,built from ongoing course

---
+++ appSwoophPractice/Model/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  appSwoophPractice
+//
+//  Created by Mac on 27/03/21.
+//  Copyright © 2021 Sanchita. All rights reserved.
+//
+
+import Foundation
---
+++ appSwoophPractice/Views/BorderedButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderedButton.swift
+//  appSwoophPractice
+//
+//  Created by Mac on 26/03/21.
+//  Copyright © 2021 Sanchita. All rights reserved.
+//
+
+import UIKit
+
+class BorderedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
